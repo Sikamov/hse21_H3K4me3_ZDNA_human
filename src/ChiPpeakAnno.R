78093e824d120d3e8962098d698bb60ca1b430c9
Kirill <sikamov.kirill@mail.ru>
2021-06-05 22:02:48 +0300

    -

--- src/ChiPpeakAnno.R
+++ src/ChiPpeakAnno.R
@@ -3,8 +3,8 @@ source('lib.R')
 ###
 
 # https://bioconductor.org/packages/release/bioc/vignettes/ChIPpeakAnno/inst/doc/quickStart.html
- BiocManager::install("ChIPpeakAnno")
- BiocManager::install("org.Hs.eg.db")
+# BiocManager::install("ChIPpeakAnno")
+# BiocManager::install("org.Hs.eg.db")
 
 
 library(ChIPpeakAnno)
@@ -15,7 +15,7 @@ library(org.Hs.eg.db)
 ###
 
 
-peaks <- toGRanges(paste0(DATA_DIR, 'H3K4me3_A549.intersect_with_DeepZ.bed'), format="BED")
+peaks <- toGRanges(paste0(DATA_DIR, 'H3K4me3_H1.intersect_with_Zhunt.bed'), format="BED")
 peaks[1:2]
 
 annoData <- toGRanges(TxDb.Hsapiens.UCSC.hg19.knownGene)
@@ -32,9 +32,9 @@ anno$symbol <- xget(anno$feature, org.Hs.egSYMBOL)
 data.frame(anno) %>% head()
 
 anno_df <- data.frame(anno)
-write.table(anno_df, file=paste0(DATA_DIR, 'H3K4me3_A549.intersect_with_DeepZ.genes.txt'),
+write.table(anno_df, file=paste0(DATA_DIR, 'H3K4me3_H1.intersect_with_ZHunt.genes.txt'),
             col.names = TRUE, row.names = FALSE, sep = '\t', quote = FALSE)
 
 uniq_genes_df <- unique(anno_df['symbol'])
-write.table(uniq_genes_df, file=paste0(DATA_DIR, 'H3K4me3_A549.intersect_with_DeepZ.genes_uniq.txt'),
+write.table(uniq_genes_df, file=paste0(DATA_DIR, 'H3K4me3_H1.intersect_with_ZHunt.genes_uniq.txt'),
             col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)

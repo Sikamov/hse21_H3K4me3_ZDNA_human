cb9c35ccab16cb445c645cfc0bdaabae1e8092d4
Kirill <sikamov.kirill@mail.ru>
2021-06-05 19:18:50 +0300

    Diagram

---
+++ src/chip_seeker.R
@@ -0,0 +1,43 @@
+
+source('lib.R')
+
+###
+
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
+# BiocManager::install("org.Hs.eg.db")
+# BiocManager::install("GO.db")
+# BiocManager::install("ChIPpeakAnno")
+
+
+library(GO.db)
+library(ChIPseeker)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(ChIPpeakAnno)
+###
+
+#NAME <- 'H3K4me3_H1.intersect_with_ZHunt'
+#NAME <- 'ZHunt'
+NAME <- 'H3K4me3_H1.ENCFF277AOQ.hg19.filtered'
+#NAME <- 'H3K4me3_H1.ENCFF456NIF.hg19.filtered'
+BED_FN <- paste0(DATA_DIR, NAME, '.bed')
+
+###
+
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
+
+pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.pdf'))
+png(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.png'))
+plotAnnoPie(peakAnno)
+dev.off()
+
+# peak <- readPeakFile(BED_FN)
+# pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
+# covplot(peak, weightCol="V5")
+# dev.off()
+# 
